de0d11811a0d8e78a813eeb12f2cff0206ba82bd
Seung Yup Han <syh62@monsterplanet.co.kr>
2016-09-26 21:05:19 +0900

    Implement opening new session and closing session

--- handlers.go
+++ handlers.go
@@ -74,6 +74,50 @@ func GenerateToken(s *Server) func(*gin.Context) {
 
 func OpenSession(s *Server) func(*gin.Context) {
 	return func(c *gin.Context) {
+    var data = struct {
+      DeviceId string `json:"deviceid" binding:"required"`
+    }{}
 
+    if err := c.BindJSON(&data); err != nil {
+      c.JSON(http.StatusBadRequest, gin.H{"status": "JSON Body is missing fields"})
+      return
+    }
+
+    deviceId := data.DeviceId
+    tokenObj := jwt.NewWithClaims(jwt.SigningMethodHS256, &jwt.StandardClaims{
+      ExpiresAt: LocalConfig.tokenLifetime,
+      Subject:   deviceId,
+    })
+    tokenStr, _ := tokenObj.SignedString([]byte(LocalConfig.tokenKey))
+
+    if _, err := s.Redis.Do("SETEX", tokenStr, LocalConfig.tokenLifetime, tokenStr); err != nil {
+      c.JSON(http.StatusInternalServerError, gin.H{"status": "Internal error"})
+      return
+    }
+
+    c.JSON(http.StatusOK, gin.H{
+      "token":    tokenStr,
+      "ttl":      LocalConfig.tokenLifetime,
+    })
 	}
+}
+
+func CloseSession(s *Server) func(*gin.Context) {
+  return func(c *gin.Context) {
+    var data = struct {
+      Token string `json:"token" binding:"required"`
+    }{}
+
+    if err := c.BindJSON(&data); err != nil {
+      c.JSON(http.StatusBadRequest, gin.H{"status": "JSON Body is missing fields"})
+      return
+    }
+
+    if _, redisErr := s.Redis.Do("DEL", data.Token); redisErr != nil {
+      c.JSON(http.StatusInternalServerError, gin.H{"status": "Internal error"})
+      return
+    }
+
+    c.JSON(http.StatusOK, gin.H{"result": "ok"})
+  }
 }
